036461eec9444498070520bc75998bf9ccdfde3d
dgw <dgw@technobabbl.es>
2017-11-01 19:00:37 -0500

    Only append year to movie notification if defined

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ app.post('/', upload.single('thumb'), function (req, res, next) {
 		if(payload.Metadata.type === 'movie') {
 			// Movies
 			msg.title = "Plex: " + payload.Account.title;
-			msg.message = payload.Metadata.title + " (" + payload.Metadata.year + ")";
+			msg.message = payload.Metadata.title;
+			if(payload.Metadata.year !== undefined) {
+				msg.message += " (" + payload.Metadata.year + ")";
+			}
 		}
 		else if(payload.Metadata.type === 'episode') {
 			// TV Shows - note padStart() requires node 8, or the --harmony flag in node 7

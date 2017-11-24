029aa04d17fcc79fd0ae7d11b13e6e4951c3035e
dgw <dgw@technobabbl.es>
2017-11-24 17:49:25 -0600

    Tabs to spaces
    Aligning blocks with tabs is such a pain (and actually impossible in
    some cases). It also tends to look ugly.
    
    Who's afraid of a few extra bytes anyway?

--- index.js
+++ index.js
@@ -8,68 +8,68 @@ var app = express();
 var push = require('pushover-notifications');
 
 var p = new push( {
-	user: process.env['PUSHOVER_USER'],
-	token: process.env['PUSHOVER_TOKEN'],
+    user: process.env['PUSHOVER_USER'],
+    token: process.env['PUSHOVER_TOKEN'],
 });
 
 app.get('*', function (req, res) {
-	res.sendStatus(405);
+    res.sendStatus(405);
 });
 
 app.post('/', upload.single('thumb'), function (req, res, next) {
-	var payload = JSON.parse(req.body.payload);
+    var payload = JSON.parse(req.body.payload);
 
-	if(payload.event === "media.play") {
-		var msg = {};
+    if(payload.event === "media.play") {
+        var msg = {};
 
-		if(payload.Metadata.type === 'movie') {
-			// Movies
-			msg.title = "Plex: " + payload.Account.title;
-			msg.message = payload.Metadata.title;
-			if(payload.Metadata.year !== undefined) {
-				msg.message += " (" + payload.Metadata.year + ")";
-			}
-		}
-		else if(payload.Metadata.type === 'episode') {
-			// TV Shows - note padStart() requires node 8, or the --harmony flag in node 7
-			msg.title = "Plex: " + payload.Account.title;
-			msg.message = "Show: " + payload.Metadata.grandparentTitle +
-					"\nEpisode: " + payload.Metadata.parentIndex +
-					"x" + payload.Metadata.index.toString().padStart(2, "0") +
-					" " + payload.Metadata.title;
-		}
-		else if(payload.Metadata.type === 'track') {
-			msg.title = "Plex: " + payload.Account.title;
-			msg.message = "Track: " + payload.Metadata.title + "\nby " +
-					payload.Metadata.grandparentTitle + "\non " +
-					payload.Metadata.parentTitle;
-		}
-		else {
-			// "Unsupported Media Type" is a good in-joke for this, isn't it?
-			res.sendStatus(415);
-			return;
-		}
+        if(payload.Metadata.type === 'movie') {
+            // Movies
+            msg.title = "Plex: " + payload.Account.title;
+            msg.message = payload.Metadata.title;
+            if(payload.Metadata.year !== undefined) {
+                msg.message += " (" + payload.Metadata.year + ")";
+            }
+        }
+        else if(payload.Metadata.type === 'episode') {
+            // TV Shows - note padStart() requires node 8, or the --harmony flag in node 7
+            msg.title = "Plex: " + payload.Account.title;
+            msg.message = "Show: " + payload.Metadata.grandparentTitle +
+                    "\nEpisode: " + payload.Metadata.parentIndex +
+                    "x" + payload.Metadata.index.toString().padStart(2, "0") +
+                    " " + payload.Metadata.title;
+        }
+        else if(payload.Metadata.type === 'track') {
+            msg.title = "Plex: " + payload.Account.title;
+            msg.message = "Track: " + payload.Metadata.title + "\nby " +
+                    payload.Metadata.grandparentTitle + "\non " +
+                    payload.Metadata.parentTitle;
+        }
+        else {
+            // "Unsupported Media Type" is a good in-joke for this, isn't it?
+            res.sendStatus(415);
+            return;
+        }
 
-		msg.title += " via " + payload.Player.title + " (" + ((payload.Player.local) ? "local" : "remote") + ")";
-		msg.url = "https://app.plex.tv/desktop#!/server/" + 
-					payload.Server.uuid + "/details/" + 
-					encodeURIComponent(payload.Metadata.key);
-		msg.url_title = "View details";
+        msg.title += " via " + payload.Player.title + " (" + ((payload.Player.local) ? "local" : "remote") + ")";
+        msg.url = "https://app.plex.tv/desktop#!/server/" + 
+                    payload.Server.uuid + "/details/" + 
+                    encodeURIComponent(payload.Metadata.key);
+        msg.url_title = "View details";
 
-		p.send( msg, function( err, result) {
-			if( err ) {
-				throw err;
-			}
+        p.send( msg, function( err, result) {
+            if( err ) {
+                throw err;
+            }
 
-			// console.log( result );
-		});
-	
-		res.sendStatus(200);
-	}
-	else {
-		// Unknown action, send a "Not Implemented" error response
-		res.sendStatus(501);
-	}
+            // console.log( result );
+        });
+    
+        res.sendStatus(200);
+    }
+    else {
+        // Unknown action, send a "Not Implemented" error response
+        res.sendStatus(501);
+    }
 });
 
 app.listen(process.env['PLEXPUSH_PORT'] || 10000);
